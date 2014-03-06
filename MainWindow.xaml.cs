d5a779cbe286ebdbba17f65d0850c1cbb0b4ff76
Molhimus <shilhog@gmail.com>
2014-03-06 15:45:18 +0200

    Tunti loppu

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿//Koodannut ja testannut toimivaksi 6.3.2014 Molhimus.
+﻿//Koodannut ja testannut toimivaksi 6.3.2014 SA.
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,6 +21,8 @@ namespace H10ADOBlanco
   /// </summary>
   public partial class MainWindow : Window
   {
+    string YhteysMerkkijono;
+    string TaulunNimi;
     public MainWindow()
     {
       InitializeComponent();
@@ -29,20 +31,34 @@ namespace H10ADOBlanco
 
     private void IniMyStuff()
     {
-      //TODO täytetään combobox asiakkaitten maitten nimillä
       //esimerkki kuinka App.Configissa oleva connectionstring luetaan
       lbMessages.Content = JAMK.ICT.Properties.Settings.Default.Tietokanta;
+      YhteysMerkkijono = JAMK.ICT.Properties.Settings.Default.Tietokanta;
+      TaulunNimi = JAMK.ICT.Properties.Settings.Default.Taulu;
+      //täytetään combobox maiden nimillä
+      cbCountries.ItemsSource = JAMK.ICT.Data.DBPlacebo.GetCitiesOfCustomersFromSQLServer(YhteysMerkkijono, TaulunNimi).DefaultView;
+      cbCountries.DisplayMemberPath = "city";
     }
 
     private void btnGet3_Click(object sender, RoutedEventArgs e)
     {
-      //TODO
+      //dgCustomers.ItemsSource = JAMK.ICT.Data.DBPlacebo.GetTestCustomers().DefaultView;
+      dgCustomers.DataContext = JAMK.ICT.Data.DBPlacebo.GetTestCustomers().DefaultView;
     }
 
     private void btnGetAll_Click(object sender, RoutedEventArgs e)
     {
-      //TODO
-    }
+      try
+      {
+        string messu = "";
+        dgCustomers.DataContext = JAMK.ICT.Data.DBPlacebo.GetAllCustomersFromSQLServer(YhteysMerkkijono, TaulunNimi, out messu);
+        lbMessages.Content = messu;
+      }
+      catch (Exception ex)
+      {
+        lbMessages.Content = ex.Message;
+      }
+     }
 
     private void btnGetFrom_Click(object sender, RoutedEventArgs e)
     {
